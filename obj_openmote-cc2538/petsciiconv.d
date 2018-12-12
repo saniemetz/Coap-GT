bb519c415624368c821cff6fe808b1e7908f3a77
saniemetz <45830852+saniemetz@users.noreply.github.com>
2018-12-12 17:30:01 -0300

    Add files via upload

---
+++ obj_openmote-cc2538/petsciiconv.d
@@ -0,0 +1,2 @@
+obj_openmote-cc2538/petsciiconv.o: ../../core/lib/petsciiconv.c
+../../core/lib/petsciiconv.c :
---
+++ obj_openmote-cc2538/res-max44009.d
@@ -0,0 +1,54 @@
+obj_openmote-cc2538/res-max44009.o: resources/res-max44009.c \
+ ../../core/./contiki.h ../../core/./contiki-version.h \
+ ../../platform/openmote-cc2538/./contiki-conf.h project-conf.h \
+ ../../platform/openmote-cc2538/./board.h ../../cpu/cc2538/./dev/gpio.h \
+ ../../cpu/cc2538/./reg.h ../../cpu/cc2538/./dev/nvic.h \
+ ../../cpu/cc2538/./cc2538_cm3.h \
+ ../../cpu/cc2538/../arm/common/CMSIS/core_cm3.h \
+ ../../cpu/cc2538/../arm/common/CMSIS/core_cmInstr.h \
+ ../../cpu/cc2538/../arm/common/CMSIS/cmsis_gcc.h \
+ ../../cpu/cc2538/../arm/common/CMSIS/core_cmFunc.h \
+ ../../core/./contiki-default-conf.h ../../core/./sys/process.h \
+ ../../core/./sys/pt.h ../../core/./sys/lc.h ../../core/./sys/lc-switch.h \
+ ../../core/./sys/cc.h ../../core/./sys/cc-gcc.h \
+ ../../core/./sys/autostart.h ../../core/./sys/process.h \
+ ../../core/./sys/timer.h ../../core/./sys/clock.h \
+ ../../core/./sys/ctimer.h ../../core/./sys/etimer.h \
+ ../../core/./sys/timer.h ../../core/./sys/etimer.h \
+ ../../core/./sys/rtimer.h ../../cpu/cc2538/./rtimer-arch.h \
+ ../../cpu/cc2538/./dev/gptimer.h ../../core/./sys/pt.h \
+ ../../core/./sys/procinit.h ../../core/./sys/loader.h \
+ ../../core/./sys/clock.h ../../core/./sys/energest.h \
+ ../../core/./sys/rtimer.h ../../apps/rest-engine/rest-engine.h \
+ ../../core/./contiki-lib.h ../../core/./contiki.h \
+ ../../core/./lib/list.h ../../core/./lib/memb.h ../../core/./lib/mmem.h \
+ ../../core/./lib/random.h ../../apps/rest-engine/rest-constants.h \
+ ../../platform/openmote-cc2538/./dev/max44009.h \
+ ../../core/./lib/sensors.h
+resources/res-max44009.c :
+ ../../core/./contiki.h ../../core/./contiki-version.h :
+ ../../platform/openmote-cc2538/./contiki-conf.h project-conf.h :
+ ../../platform/openmote-cc2538/./board.h ../../cpu/cc2538/./dev/gpio.h :
+ ../../cpu/cc2538/./reg.h ../../cpu/cc2538/./dev/nvic.h :
+ ../../cpu/cc2538/./cc2538_cm3.h :
+ ../../cpu/cc2538/../arm/common/CMSIS/core_cm3.h :
+ ../../cpu/cc2538/../arm/common/CMSIS/core_cmInstr.h :
+ ../../cpu/cc2538/../arm/common/CMSIS/cmsis_gcc.h :
+ ../../cpu/cc2538/../arm/common/CMSIS/core_cmFunc.h :
+ ../../core/./contiki-default-conf.h ../../core/./sys/process.h :
+ ../../core/./sys/pt.h ../../core/./sys/lc.h ../../core/./sys/lc-switch.h :
+ ../../core/./sys/cc.h ../../core/./sys/cc-gcc.h :
+ ../../core/./sys/autostart.h ../../core/./sys/process.h :
+ ../../core/./sys/timer.h ../../core/./sys/clock.h :
+ ../../core/./sys/ctimer.h ../../core/./sys/etimer.h :
+ ../../core/./sys/timer.h ../../core/./sys/etimer.h :
+ ../../core/./sys/rtimer.h ../../cpu/cc2538/./rtimer-arch.h :
+ ../../cpu/cc2538/./dev/gptimer.h ../../core/./sys/pt.h :
+ ../../core/./sys/procinit.h ../../core/./sys/loader.h :
+ ../../core/./sys/clock.h ../../core/./sys/energest.h :
+ ../../core/./sys/rtimer.h ../../apps/rest-engine/rest-engine.h :
+ ../../core/./contiki-lib.h ../../core/./contiki.h :
+ ../../core/./lib/list.h ../../core/./lib/memb.h ../../core/./lib/mmem.h :
+ ../../core/./lib/random.h ../../apps/rest-engine/rest-constants.h :
+ ../../platform/openmote-cc2538/./dev/max44009.h :
+ ../../core/./lib/sensors.h :
